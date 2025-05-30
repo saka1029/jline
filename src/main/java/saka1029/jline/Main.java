530bccb28371c09ed3111ea5e6aace09b7685a1d
saka1029 <saka1029@gmail.com>
2025-05-30 13:27:19 +0900

    simple

--- src/main/java/saka1029/jline/Main.java
+++ src/main/java/saka1029/jline/Main.java
@@ -1,7 +1,34 @@
 package saka1029.jline;
 
+import org.jline.reader.Parser;
+import org.jline.reader.impl.DefaultParser;
+import java.io.IOException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        // JLine terminal の準備
+        Terminal terminal = TerminalBuilder.builder().system(true).build();
+
+        // Parser の準備
+        Parser p = (Parser) new DefaultParser();
+        LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).parser(p).build();
+
+        // REPL
+        while (true) {
+            String line = lineReader.readLine("input > ");
+
+            String[] exitCommands = {":exit"};
+            for (String cmd : exitCommands) {
+                if (line.contains(cmd)) {
+                    return;
+                }
+            }
+
+            System.out.println(line);
+        }
     }
 }
